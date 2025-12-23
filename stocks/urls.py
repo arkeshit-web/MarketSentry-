b6f4490496cf1e79368c4a8d2539ff07557b1b8d
ARKESH BAIDYA <arkesh.it@gmail.com>
2025-12-24 00:03:55 +0530

    new commit

--- stocks/urls.py
+++ stocks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.StockListView.as_view(), name='stock-list'),
-    path('<str:ticker>/', views.StockDetailView.as_view(), name='stock-detail'),
-    path('api/compare/', views.compare_stocks, name='stock-compare'),
     path('setup/ingest/', views.ingest_data_view, name='ingest-data'),
+    path('', views.StockListView.as_view(), name='stock-list'),
+    path('api/compare/', views.compare_stocks, name='stock-compare'),
+    path('<str:ticker>/', views.StockDetailView.as_view(), name='stock-detail'),
 ]
